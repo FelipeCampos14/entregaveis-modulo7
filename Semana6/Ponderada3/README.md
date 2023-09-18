c4413ca53d2227366efa717bbc70a177a731f508
Felipe Campos <Felipe.Campos@sou.inteli.edu.br>
2023-09-17 22:41:35 -0300

    Terminei

--- Semana6/Ponderada3/README.md
+++ Semana6/Ponderada3/README.md
@@ -1,3 +1,25 @@
 # Documentação 
 
-Incialemnte foi feito no Colab todo o processamento de dados e escolha do modelo uma vez que lá é possível 
+## Pipeline e Modelagem
+Inicialemnte foi feito no Colab todo o processamento de dados e escolha do modelo uma vez que lá é possível utilizar o Pycaret. Portanto há dois notebooks, um com AutoML chamado Colab e outro utilizando a biblioteca scikit-learn, chamado vscode. Há também um arquivo vscode.py que serve apenas para poder exportar algumas variáveis do notebook para a api.
+
+Ambos notebooks são praticamente iguais nas partes de pré-processamento, exploração dos dados e feature engineering. A pipeline que se seguiu foi basicamente de repor dados NaN por dado médios — mediana para valores contínuos e moda para dados categóricos, localizar e corrigir dados inconsistentes, remover colunas com baixa correlação com a variável target e colunas com alta correlação com a variável target, mas com alta correlação entre si.
+
+Já em relação a parte de modelo, os dois notebooks se diferem. O notebook do colab, como dito antes, foi feito utilizando AutoML, obtendo diversos resultados com overfitting, com 0,999 de r2 score. Consequentemente, foi escolhido o primeiro algpritmo que não tivesse um grau de overfitting tão alto. Portanto o modelo usado foi de Lasso.
+
+A coluna target selecionada foi 'rank', com o objetivo de inputar as informações do canal e descobrir em que lugar ele ficaria no ranking.
+
+No notebook vscode.ipynb, utilzou-se o modelo previamente escolhido, utilizando a biblioteca scikit-learn. Além disso, o modelo de AutoML do Colab continha uma remoção de 5% dos outliers utilizando o modelo de Isolation Forest, que também foi replicado no notebook vscode. Também aplicou-se um teste de validação cruzada para testar se o algoritmo não estava com sobreajuste, o que não aparentou ter. Ao final o modelo é salvo em um arquivo pkl para ser usado pela API.
+
+Em relação à API, é carregado o modelo e importadas algumas variáveis para contruir uma dataframe que possa ser lido pelo modelo. Essa informações são processadas na rota post 'predict', que pode ser acessada pelo endereço das aplicação + '/docs' na guia.
+
+## Executando os containers
+passo 1:
+```
+docker pull felipecampos14/ponderada3:latest
+```
+
+passo 2:
+```
+docker run 8080:80 felipecampos14/ponderada3:latest
+```
--- Semana6/Ponderada3/api.py
+++ Semana6/Ponderada3/api.py
@@ -3,7 +3,34 @@ from sklearn import linear_model
 from fastapi import FastAPI
 import uvicorn
 import joblib
-from modelo import join_all, x
+from typing import List
+
+
+join_all_columns = ['rank', 'subscribers', 'video views','video_views_for_the_last_30_days', 'creation', 'Afghanistan',
+       'Andorra', 'Argentina', 'Australia', 'Bangladesh', 'Barbados', 'Brazil',
+       'Canada', 'Chile', 'China', 'Colombia', 'Cuba', 'Ecuador', 'Egypt',
+       'El Salvador', 'Finland', 'France', 'Germany', 'India', 'Indonesia',
+       'Iraq', 'Italy', 'Japan', 'Jordan', 'Kuwait', 'Latvia', 'Malaysia',
+       'Mexico', 'Morocco', 'Netherlands', 'Pakistan', 'Peru', 'Philippines',
+       'Russia', 'Samoa', 'Saudi Arabia', 'Singapore', 'South Korea', 'Spain',
+       'Sweden', 'Switzerland', 'Thailand', 'Turkey', 'Ukraine',
+       'United Arab Emirates', 'United Kingdom', 'United States', 'Venezuela',
+       'Vietnam', 'Animals', 'Autos', 'Comedy', 'Education', 'Entertainment',
+       'Film', 'Games', 'Howto', 'Music', 'News', 'Nonprofit', 'People',
+       'Sports', 'Tech']
+
+x_columns = ['subscribers', 'video views','video_views_for_the_last_30_days', 'creation', 'Afghanistan',
+       'Andorra', 'Argentina', 'Australia', 'Bangladesh', 'Barbados', 'Brazil',
+       'Canada', 'Chile', 'China', 'Colombia', 'Cuba', 'Ecuador', 'Egypt',
+       'El Salvador', 'Finland', 'France', 'Germany', 'India', 'Indonesia',
+       'Iraq', 'Italy', 'Japan', 'Jordan', 'Kuwait', 'Latvia', 'Malaysia',
+       'Mexico', 'Morocco', 'Netherlands', 'Pakistan', 'Peru', 'Philippines',
+       'Russia', 'Samoa', 'Saudi Arabia', 'Singapore', 'South Korea', 'Spain',
+       'Sweden', 'Switzerland', 'Thailand', 'Turkey', 'Ukraine',
+       'United Arab Emirates', 'United Kingdom', 'United States', 'Venezuela',
+       'Vietnam', 'Animals', 'Autos', 'Comedy', 'Education', 'Entertainment',
+       'Film', 'Games', 'Howto', 'Music', 'News', 'Nonprofit', 'People',
+       'Sports', 'Tech']
 
 # Create the app
 app = FastAPI()
@@ -11,38 +38,37 @@ app = FastAPI()
 # Load trained Pipeline
 model = joblib.load("modelo.pkl")
 
-# Prepara uma entrada para predição
-data = pd.DataFrame([['10000000', '6275', '10000000', '5000000','United States','Games']])
-data.columns = ['video_views_for_the_last_30_days','creation','subscribers','video views','Country','channel_type']
-data[data['Country']] = 1
-data[data['channel_type']] = 1
-
-# Criação dos dataframe com os outros dados do one hot encoding
-columns = list(set(join_all.columns) - set(data.columns))
-zero_array = []
-for i in range(len(columns)):
-  zero_array.append(0)
-data2 = pd.DataFrame([zero_array])
-data2.columns = columns
-
-# Join dos dois dataframes
-data_prediction = data.join(data2)
-
-# tirando o y e colunas do one-hot
-columns_to_drop = ['Country','channel_type','rank']
-data_prediction = data_prediction.drop(columns=columns_to_drop)
-data_prediction = data_prediction[x.columns]
 
 # Define predict function
-@app.post("/predict")
-def predict(data = data_prediction):
-    predictions = model.predict(data)
-    return predictions
+@app.get("/predict", response_model=List[float])
+def predict():
+    # Prepara uma entrada para predição
+    data = pd.DataFrame([['10000000', '6175', '10000000', '5000000','United States','Games']])
+    data_colunas = ['video_views_for_the_last_30_days','creation','subscribers','video views','Country','channel_type']
+    data.columns = data_colunas
+    data[data['Country']] = 1
+    data[data['channel_type']] = 1
+
+    # Criação dos dataframe com os outros dados do one hot encoding
+    columns = list(set(join_all_columns) - set(data.columns))
+    zero_array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    data2 = pd.DataFrame([zero_array])
+    data2.columns = columns
+
+    # Join dos dois dataframes
+    data_prediction = data.join(data2)
+
+    # tirando o y e colunas do one-hot
+    columns_to_drop = ['Country','channel_type','rank']
+    data_prediction = data_prediction.drop(columns=columns_to_drop)
+    data_prediction = data_prediction[x_columns]
+    predictions = model.predict(data_prediction)
+    return predictions.tolist()
 
 # Define predict function
 @app.get("/")
 def ola_mundo():
-    return "hello world"
+    return 'hi'
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
