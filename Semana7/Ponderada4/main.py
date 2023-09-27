5019111f2f6034d78310c49eb0db5510dbbcc80f
Felipe Campos <Felipe.Campos@sou.inteli.edu.br>
2023-09-27 14:20:06 -0300

    First commit

---
+++ Semana7/Ponderada4/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def hello_world():
+    return "Hello World!"
+
+@app.get("/user_login")
+def user_login():
+    return 
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
